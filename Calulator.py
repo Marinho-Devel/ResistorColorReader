f3abd072bbbc13066735397617179e74de15e785
Túlio Marinho <tulio.smarinho@gmail.com>
2025-02-24 23:15:41 -0300

    Construction of calculator.

--- Calulator.py
+++ Calulator.py
@@ -1,26 +1,55 @@
 print("RESISTOR COLOR CALCULATOR:")
 print("Choice option: 1 for calculate by colors or 2 to get color by value")
-choice = input("insert the option: ")
+
+def colors():
+    return ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'grey', 'white']
+
+def colorsCalc():
+    #tolerance = ['silver', 'gold']
+    colorsValues = colors()         #ASSINGS THE RETURN VALUE OF A FUNCTION TO A VARIABLE
+    stripe = [1, 2, 3, 4]
+    resistorValue = [1, 2, 3, 4]
+    resistence = 0
+    for i in range(len(stripe)):
+        stripe[i] = input(f"insert the color of {stripe[i]} stripe: ")
+
+        if stripe[i] in colorsValues:
+            resistorValue[i] = colorsValues.index(stripe[i])
+        else:
+            print("invalid color.")
+
+    resistence = (resistorValue[0] * 10) + resistorValue[1]
+    resistence = resistence * (10 ** resistorValue[2])
+    print("the resistence in Ohms is:", resistence)
+    print("resistor value:", resistorValue)
+
+
+
+    print(f"color: {colorsValues[2]}")
+
+def valueCalc(colorsValues):
+    print(f"color: {colorsValues[3]}")
+
 def menu():
+    choice = input("insert the option: ")
+
     match choice:
+
         case '1':
             colorsCalc()
 
         case '2':
             valueCalc()
 
+        case '3':
+            print("exiting")
+
         case _:
             print("invalid option.")
 
-    menu()
+menu()
 
-def dictionary():
-    colorsValues = {'black': 0, 'brown': 1, 'red': 2, 'orange': 3, 'yellow': 4, 'green': 5, 'blue': 6, 'purple': 7, 'grey': 8, 'white': 9}
-    return colorsValues
 
-def colorsCalc(colorsValues):
-    print(f"color: {colorsValues[2]}")
 
-def valueCalc(colorsValues):
-    print(f"colof: {colorsValues[3]}")
+
 
